c40e29f319d2b2857bcc7816dd0f8d6b2138b7d4
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 20:44:16 -0800

    added age updating

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ var diff = date_2.getTime() - age.getTime()
 
 window.onload = function() {
     document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.
-    
+
     I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
 }
+
+const age_interval = setInterval(function() {
+    var diff = date_2.getTime() - age.getTime();
+    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.
+
+    I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
+}, 100);
 
 let count = 0;
 let colors = ["black", "#673147", "pink", "#E35335", "yellow", "white"];
