edabe8b84e53420f647d5b8dbb89a5014441b13e
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 18:23:44 -0800

    Added vertical center

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var date_2 = new Date();
 var diff = date_2.getTime() - age.getTime()
 
 window.onload = function() {
-    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.`
+    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.
+    
+    I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
 }
 
 let count = 0;
