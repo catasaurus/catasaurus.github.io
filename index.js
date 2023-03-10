8b5f2f4649a92fed9410deb7b669c287a41f98f2
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 17:14:07 -0800

    Fixed date again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var age = new Date('1/1/2010');
 var date_2 = new Date();
-var diff = age.getTime() - date_2.getTime()
+var diff = date_2.getTime() - age.getTime()
 
 window.onload = function() {
     document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.`
