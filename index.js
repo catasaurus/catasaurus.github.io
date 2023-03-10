4394944459053cd66e14993aef6471db92b877d9
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 17:49:17 -0800

    Added changine background colors

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ var diff = date_2.getTime() - age.getTime()
 window.onload = function() {
     document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.`
 }
+
+let count = 0;
+let colors = ["black", "red", "pink", "orange", "yellow", "white"];
+const interval = setInterval(function() {
+    document.getElementById("main").style.background = colors[count];
+    if (count < 5) {
+        count += 1;
+    } else {
+        count = 0;
+    };
+}, 1000);
