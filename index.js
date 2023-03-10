ea15a982ade3d9b25614e253c7727d84744a8793
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 17:08:44 -0800

    Fixed date

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var date_2 = new Date();
 var diff = age.getTime() - date_2.getTime()
 
 window.onload = function() {
-    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 365} year old person. I am passionate about programming and AI, and basically anything relating to that.`
+    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.`
 }
