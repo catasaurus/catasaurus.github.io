89c41a5ceadd20fb0955f0fd7231c1341f019d7c
Sebastian Larson <sebastianlarson22@gmail.com>
2023-03-09 20:55:23 -0800

    fixed stuff

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ window.onload = function() {
     I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
 }
 
-const age_interval = setInterval(function() {
-    var diff = date_2.getTime() - age.getTime();
-    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.
-
-    I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
-}, 100);
-
 let count = 0;
 let colors = ["black", "#673147", "pink", "#E35335", "yellow", "white"];
 const interval = setInterval(function() {
@@ -28,4 +21,9 @@ const interval = setInterval(function() {
     } else {
         count = 0;
     };
+
+    var diff = date_2.getTime() - age.getTime();
+    document.getElementById("age").innerHTML=`Hi! I am catasaurus, a ${diff / 3.154e+10} year old person. I am passionate about programming and AI, and basically anything relating to that.
+
+    I am proficient in Python, C/C++, and Javascript. Generally I work on projects that involve Machine Learning, NLP, Data science, and sometimes some web development.`
 }, 2000);
